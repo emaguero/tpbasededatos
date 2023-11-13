03c068ff09d5db16dfb048aea68844640e5b77f3
emaguero <aguero.emiliano@gmail.com>
2023-11-13 16:05:17 -0300

    create, insert y describe

---
+++ backup-oradores.sql
@@ -0,0 +1,12 @@
+REM INSERTING into EXPORT_TABLE
+SET DEFINE OFF;
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('1','Santiago','Bilinkis','santiago@bilinkis.com','IA',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('2','Enrique','Piedra','enrique@piedra.com','Desarrollo',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('3','Facundo','Manes','facundo@manes.com','El cerebro',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('4','Ricardo',']Iorio','ricadro@almafuerte.com','El metal Argentino',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('5','Cesar','Fuentes Rodriguez','avecesar@avecesar.com','Politicamente incorrecto',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('6','Alejandro','Fantino','alejandro@fantibio.com','Politicamente incorrecto',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('7','Esteban','Quito','esteban@quito.com','Politicamente incorrecto',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('8','Emiliano','Aguero','emiliano@aguero.com','Politicamente incorrecto',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('9','Ulises','Aguero','ulises@aguero.com','Politicamente incorrecto',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
+Insert into EXPORT_TABLE (ID_ORADOR,NOMBRE,APELLIDO,MAIL,TEMA,FECHA_ALTA) values ('10','Aldo','Duarte','aldo@querido.com','Politicamente incorrecto',to_date('10/12/2024 12:00','DD/MM/RRRR HH:MI'));
